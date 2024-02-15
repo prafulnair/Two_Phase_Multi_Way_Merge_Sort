7af4e84b41e84f679519d71e0efb3029759885ff
Praful Nair <51130135+prafulnair@users.noreply.github.com>
2024-02-15 05:49:00 -0500

    Generate runs dynamically
    This update involves creating a number of runs based on the data, and populating these runs with the records. For efficiency, we are using a dictionary to store employee id as key, and rest of the data (String data) as values
    
    This will enable us to sort based on the key value

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Coded by Praful Peethambaran Nair (40226483)
 Database Schema Details
 Employee ID  = 8 chars
 Employee Name = 25 chars
-LastUpdate = 10 chars
+LastUpdate = 10 chars // does not exist, so ignore this
 Gender = 1 char (0 or 1)
 Department = 3 chars
 Social Insurance Number = 9 chars
@@ -15,16 +15,42 @@ Address = 43 chars.
 """
 
 """
-We can store each record as a tuple of two data. The first part would contain 123456782024-01-15John, 
+We can store each record as a tuple of two data. The first part would contain 123456782024, which is the employee id
 and the second part would contain the rest of the data, 
-12223333333331455 de Maisonneuve West, Montreal, QC, H3G 1M8, Canada
-
-This is because 
+12223333 de Maisonneuve West, Montreal, QC, H3G 1M8, Canada
 """
 
 
 import sys
 from tqdm import tqdm
+
+def generate_runs(main_list, run_size):
+    runs = [] 
+    total_records = len(main_list)
+    num_runs = total_records // run_size
+    remainder = total_records % run_size
+
+    for i in range(num_runs):
+        run = {} # Creating dictionary to store emp ID as key and rest of the string as value
+        for j in range(run_size):
+            record = main_list[i*run_size+j] # extracting 10 / 20 records for the runs
+            key = int(record[:8].strip()) # extracting the employee ID
+            value = record[8:]
+            run[key] = value
+        runs.append(run)
+
+    if remainder > 0:
+        run = {}
+        for i in range(remainder):
+            record = main_list[num_runs * run_size + i]
+            key = int(record[:8].strip())
+            value = record[8:]
+            run[key] = value
+        runs.append(run)
+
+    return runs 
+
+    # Now you have variables run_1, run_2, ..., run_n, each containing an empty list
 
 def read_data_main2(filename1, filename2):
     # Creating the main buffer. main_list will contain data from both the files..
@@ -70,6 +96,15 @@ if __name__ == "__main__":
     # uncomment the following for loop if you want to print the loaded data
     # for records in main_list:
     #     print(records)
-    print("TOTAL Records read and stored in main buffer: ",rows)
+    # print("TOTAL Records read and stored in main buffer: ",rows)
 
-    create_runs(main_list)
+    runs = generate_runs(main_list, run_size=10)
+
+    for run in runs:
+        for keys in run.keys():
+            print(keys)
+
+    print(len(runs))
+
+
+
